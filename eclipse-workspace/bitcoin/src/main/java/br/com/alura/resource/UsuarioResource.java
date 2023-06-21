ad803721adb5811fe31a95d5b8d028d28a417760
jonnathanweber2 <jonnathanwebersn@gmail.com>
2023-06-21 20:04:09 -0300

    adicionado regras de ordens e encriptacao de senha

---
+++ eclipse-workspace/bitcoin/src/main/java/br/com/alura/resource/UsuarioResource.java
@@ -0,0 +1,22 @@
+package br.com.alura.resource;
+
+import br.com.alura.model.Usuario;
+import jakarta.annotation.security.PermitAll;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.core.MediaType;
+
+
+@Path("/usuarios")
+public class UsuarioResource {
+
+    @POST
+    @PermitAll
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void inserir(Usuario usuario) {
+        Usuario.adicionar(usuario);
+    }
+}
